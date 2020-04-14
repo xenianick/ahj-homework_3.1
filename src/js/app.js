8656b19fe8877153314d48a8a9722e21dfdabd47
Ksenia <ksenia@MacBook-Pro-Nikita.local>
2020-04-14 18:53:03 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -33,23 +33,35 @@ pointsContainerEl.appendChild(scoresEl);
 pointsContainerEl.appendChild(missesEl);
 body.insertBefore(pointsContainerEl, body.lastChild);
 
+let isClicked = false;
 
+function countMisses() {
+  if (isClicked) {
+    isClicked = false;
+    return;
+  }
+  misses += 1;
+  missesEl.innerHTML = `<p>Пропущено: ${misses}</p>`;
+  if (misses >= 5) {
+    clearTimeout(timerId);
+    showModal('Вы проиграли &#128531', 'Жалко', body);
+  }
+}
+
+changePlace(icon, cellsQuantity, fieldEl);
 setInterval(changePlace, 1000, icon, cellsQuantity, fieldEl);
+const timerId = setInterval(countMisses, 1000);
 
 const cells = Array.from(fieldEl.querySelectorAll('.cell'));
 cells.forEach((item) => {
   item.addEventListener('click', () => {
     if (icon.closest('.cell') === item) {
+      isClicked = true;
       scores += 1;
       scoresEl.innerHTML = `<p>Поймано: ${scores}</p>`;
       if (scores >= 5) {
+        clearTimeout(timerId);
         showModal('Победа! &#127881;', 'Ну и отлично', body);
-      }
-    } else {
-      misses += 1;
-      missesEl.innerHTML = `<p>Пропущено: ${misses}</p>`;
-      if (misses >= 5) {
-        showModal('Вы проиграли &#128531', 'Жалко', body);
       }
     }
   });
